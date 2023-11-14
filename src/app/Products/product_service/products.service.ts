2c82fe2e926bf868640f5b949664601d6b1b9a87
ntokozo-nhlangulela <nnhlangulela@jhb.dvt.co.za>
2023-11-14 13:21:24 +0200

    get all products service call with model

--- src/app/Products/product_service/products.service.ts
+++ src/app/Products/product_service/products.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
+import { Product } from '../model/product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getAllProducts() {
+    return this.http.get<Product[]>('https://fakestoreapi.com/products').pipe(
+      map((res: Product[]) => {
+        console.log(res);
+        return res;
+      })
+    );
+  }
 }
